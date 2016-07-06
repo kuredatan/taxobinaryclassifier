f87450fdc4a287c1cccc2f89638d3a94df97d6cd
Clémence <reda.clmc@live.fr>
2016-07-06 11:58:25 +0200

    Improved parsing

--- parsingMatch.py
+++ parsingMatch.py
@@ -1,39 +1,46 @@
 from time import time
 import sys as s
+import re
 
 from misc import sanitize
+
+integer = re.compile("[0-9]+")
 
 #Returns the pair (identifier of patient a.k.a. @filename,list of identifiers of sequences matching a read in this patient)
 def parseMatch(filename):
     allSequences = []
-    file_match = open("meta/match/" + filename + ".match","r")
-    lines = file_match.readlines()
-    file_match.close()
+    fo = open("meta/match/" + filename + ".match","r")
+    r = fo.readlines()
+    numberRead = 0
     #Each line corresponds to a read of this patient
-    for read in lines:
+    for read in r:
+        print numberRead
         lsDirty = read.split(" ")
         lsClean = []
         #Last string is "\n"
-        for string in lsDirty[:-1]:
-            lsClean.append(sanitize(string))
-        if len(lsClean) < 1:
-            print "\n/!\ ERROR: MATCH parsing error:",len(lsClean),"."
-            raise ValueError
-        print lsClean[0]
-        allSequences += lsClean[1:]
+        for string in lsDirty[1:-1]:
+            s = sanitize(string)
+            if integer.match(s):
+                lsClean.append(int(s))
+        if len(lsClean) > 0:
+            allSequences += lsClean
+        numberRead += 1
+    print allSequences[:100]
+    fo.close()
     return (filename,allSequences)
 
 #Returns the list @allMatches such as @allMatches[i] is a pair (identifier of patient,list of identifiers of sequences matching a read in this patient) 
 def parseAllMatch(filenames):
     allMatches = []
     start = time()
-    try:
-        for filename in filenames:
+    for filename in filenames:
+        try:
+            print filename
             if filename:
                 allMatches.append(parseMatch(filename))
-    except IOError:
-        print "\nERROR: Maybe one of these filenames",filenames,"does not exist in \"meta/matches\" folder\n"
-        s.exit(0)
+        except IOError:
+            print "\nERROR: Maybe the filename",filename,".match does not exist in \"meta/matches\" folder\n"
+            s.exit(0)
     end = time()
     print "TIME .match:",(end-start)
-    return allMatches
+    #return allMatches
